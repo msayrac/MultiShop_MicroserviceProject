50fa2e9e72ec958e760d79d63006c386dc9e60f6
msayrac <msayracode@gmail.com>
2024-11-05 21:29:56 +0300

    Category Api tested

--- Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -30,7 +30,7 @@ namespace MultiShop.Catalog.Services.CategoryServices
 		}
 		public async Task DeleteCategoryAsync(string id)
 		{
-			await _categoryCollection.DeleteOneAsync(x => x.CategoryID == id);
+			await _categoryCollection.DeleteOneAsync(x => x.CategoryId == id);
 		}
 
 		public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
@@ -42,7 +42,7 @@ namespace MultiShop.Catalog.Services.CategoryServices
 
 		public async Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
 		{
-			var values = await _categoryCollection.Find(x => x.CategoryID == id).FirstOrDefaultAsync();
+			var values = await _categoryCollection.Find(x => x.CategoryId == id).FirstOrDefaultAsync();
 
 			return _mapper.Map<GetByIdCategoryDto>(values);
 
@@ -51,7 +51,7 @@ namespace MultiShop.Catalog.Services.CategoryServices
 		public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
 		{
 			var values = _mapper.Map<Category>(updateCategoryDto);
-			await _categoryCollection.FindOneAndReplaceAsync(x => x.CategoryID == updateCategoryDto.CategoryID, values);
+			await _categoryCollection.FindOneAndReplaceAsync(x => x.CategoryId == updateCategoryDto.CategoryId, values);
 		}
 	}
 }
